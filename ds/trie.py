b846fc81e0a5af94150523e67ff341d333f47f18
vincent <ldg55d@gmail.com>
2018-10-18 11:51:20 +0900

    trie

--- ds/trie.py
+++ ds/trie.py
@@ -10,12 +10,32 @@ class Trie:
   def __init__(self):
     self.root = Node()
 
-  def search(self, val):
-    pass
- 
-  def insert(self, val):
-    pass
+  def search(self, s):
+    node = self.root
+    for i, ch in enumerate(s):
+      if ch not in node.children:
+        return
 
+      node = node.children[ch]
+      if node.end:
+        print(s[:i+1])
+    
+    self.print_tree(s[:i+1], node) 
+
+  def print_tree(self, word, node):
+    if node.end:
+      print(word)
+
+    for ch in node.children:
+      self.print_tree(word + ch, node.children[ch])
+      
+  def insert(self, s):
+    node = self.root
+    for ch in s:
+      if ch not in node.children:
+        node.children[ch] = Node()
+      node = node.children[ch]
+    node.end = True
 
 
 trie = Trie()
@@ -27,3 +47,11 @@ trie.insert('thaw')
 trie.insert('bye')
 trie.insert('any')
 trie.insert('answer')
+
+trie.search('an')
+print()
+trie.search('th')
+print()
+trie.search('tha')
+print()
+trie.search('b')
