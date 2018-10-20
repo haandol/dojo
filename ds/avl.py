16dc732ae158e3f50a62fb17b7fef6f277dde6a9
vincent <ldg55d@gmail.com>
2018-10-20 19:27:55 +0900

    add avl delete

--- ds/avl.py
+++ ds/avl.py
@@ -9,16 +9,13 @@ class Node:
 
 
 def insert(node, val):
+  if not node:
+    return Node(val)
+
   if val <= node.val:
-    if node.left:
-      node.left = insert(node.left, val)
-    else:
-      node.left = Node(val)
+    node.left = insert(node.left, val)
   elif node.val < val:
-    if node.right:
-      node.right = insert(node.right, val)
-    else:
-      node.right = Node(val)
+    node.right = insert(node.right, val)
 
   node.height = 1 + max(get_height(node.left), get_height(node.right))
 
@@ -27,13 +24,13 @@ def insert(node, val):
   # LL
   if balance > 1 and val < node.left.val:
     return rotate_right(node)
-  # RR
-  if balance < -1 and node.right.val < val:
-    return rotate_left(node)
   # LR
   if balance > 1 and node.left.val < val:
     node.left = rotate_left(node.left)
     return rotate_right(node)
+  # RR
+  if balance < -1 and node.right.val < val:
+    return rotate_left(node)
   # RL
   if balance < -1 and val < node.right.val:
     node.right = rotate_right(node.right)
@@ -41,14 +38,18 @@ def insert(node, val):
 
   return node
 
-def get_height(node):
-  if node is None:
-    return 0
-
-  return node.height
 
 def get_balance(node):
+  if not node:
+    return 0
   return get_height(node.left) - get_height(node.right)
+
+
+def get_height(node):
+  if not node:
+    return 0
+  return node.height
+
 
 def rotate_left(x):
   y = x.right
@@ -62,6 +63,7 @@ def rotate_left(x):
 
   return y
 
+
 def rotate_right(x):
   y = x.left
   c = y.right
@@ -74,11 +76,55 @@ def rotate_right(x):
 
   return y
 
-def delete(node):
-  pass
+
+def delete(node, val):
+  if not node:
+    return
+
+  if val < node.val:
+    node.left = delete(node.left, val)
+  elif node.val < val:
+    node.right = delete(node.right, val)
+  
+  if not node.left:
+    return node.right 
+  elif not node.right:
+    return node.left
+  
+  successor = get_successor(node.right)
+  node.val = successor.val
+  node.right = delete(node.right, successor.val)
+
+  node.height = 1 + max(get_height(node.left), get_height(node.right))
+
+  balance = get_balance(node)
+
+  # LL
+  if balance > 1 and 0 <= get_balance(node.left):
+    return rotate_right(node)
+  # LR
+  if balance > 1 and get_balance(node.left) < 0:
+    node.left = rotate_left(node.left)
+    return rotate_right(node)
+  # RR
+  if balance < -1 and get_balance(node.right) <= 0:
+    return rotate_left(node)
+  # RL
+  if balance < -1 and 0 < get_balance(node.right):
+    node.right = rotate_right(node.right)
+    return rotate_left(node)
+
+  return node
+
+
+def get_successor(node):
+  while node.left:
+    node = node.left
+  return node
+
 
 def preorder(node):
-  if node is None:
+  if not node:
     return
 
   print(node.val, node.height)
@@ -93,5 +139,9 @@ if __name__ == '__main__':
   root = insert(root, 40) 
   root = insert(root, 50) 
   root = insert(root, 25) 
-
   preorder(root)
+  print()
+
+  root = delete(root, 40)
+  preorder(root)
+  print()
