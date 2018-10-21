1730113f52b560e51e9c4ee1e584f44df063f34e
vincent <ldg55d@gmail.com>
2018-10-21 22:54:29 +0900

    add powerset

---
+++ dp/powerset.py
@@ -0,0 +1,14 @@
+# https://www.geeksforgeeks.org/power-set/amp/
+
+def powerset(arr, n):
+  n = pow(2, n)
+
+  for i in range(n):
+    for j in range(n):
+      if 0 < (i & (1 << j)):
+        print(arr[j], end='')
+    print()
+
+
+arr = ['a', 'b', 'c']; 
+powerset(arr, 3); 
