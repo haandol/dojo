ef698639d44cd71fe260e95dc68eae8b3db2b74a
vincent <ldg55d@gmail.com>
2018-10-22 22:53:49 +0900

    add permutation

--- dp/permutation.py
+++ dp/permutation.py
@@ -1,13 +1,13 @@
 def permute(arr, i, n):
-  if i == n-1:
-    print(''.join(arr))
+  if i == n - 1:
+    print(arr)
     return
-    
+
   for j in range(i, n):
     arr[i], arr[j] = arr[j], arr[i]
-    permute(arr, i+1, n)
+    permute(arr, i + 1, n)
     arr[i], arr[j] = arr[j], arr[i]
 
 
-arr = 'ABC'
-permute(list(arr), 0, len(arr))
+arr = ['A', 'B', 'C']
+permute(arr, 0, len(arr))
