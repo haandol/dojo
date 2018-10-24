f51d308c174b43e467b3ea0b2afd9c5f6503eccc
Lee <dongklee@ea.com>
2018-10-24 18:02:21 +0900

    add insertion

--- sort/insertion.py
+++ sort/insertion.py
@@ -2,14 +2,11 @@ def sort(arr):
   n = len(arr)
   for i in range(1, n):
     val = arr[i]
-    j = i
-    while 0 < j:
-      if val < arr[j-1]:
-        arr[j] = arr[j-1]
+    j = i - 1
+    while 0 <= j and val < arr[j]:
+        arr[j+1] = arr[j]
         j -= 1
-      else:
-        break
-    arr[j] = val
+    arr[j+1] = val
 
 arr = [12, 11, 13, 5, 6]
 sort(arr)
