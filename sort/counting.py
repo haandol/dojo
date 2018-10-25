adbdada0c06e7431b5baab8920a889f8c94bc020
Lee <dongklee@ea.com>
2018-10-25 13:29:47 +0900

    modify counting

--- sort/counting.py
+++ sort/counting.py
@@ -1,18 +1,20 @@
+# https://www.geeksforgeeks.org/counting-sort/
+
 def sort(arr):
   n = len(arr)
-  data = [0] * (max(arr) + 1)
+  result = [-1] * n
+  counts = [0] * (max(arr) + 1)
+
+  for el in arr:
+    counts[el] += 1
+  
+  for i in range(1, len(counts)):
+    counts[i] += counts[i-1]
 
   for i in range(n):
-    data[arr[i]] += 1
+    result[counts[arr[i]] - 1] = arr[i]
+    counts[arr[i]] -= 1
 
-  for i in range(len(data) - 1):
-    data[i+1] += data[i]
-
-  result = [-1] * n
-  for i in range(n - 1, -1, -1):
-    result[data[arr[i]] - 1] = arr[i]
-    data[arr[i]] -= 1
-  
   return result
 
 
