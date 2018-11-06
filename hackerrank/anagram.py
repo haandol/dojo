d4314d0fceeb988cd2efcb493f3965b3a17a5132
Lee <dongklee@ea.com>
2018-11-06 17:49:25 +0900

    add 50 answer

--- hackerrank/anagram.py
+++ hackerrank/anagram.py
@@ -1,35 +1,31 @@
 # https://www.hackerrank.com/challenges/sherlock-and-anagrams
 
-def get_substrings(s, l, n):
+def get_substrings(s, word_n, n):
   words = []
-  for i in range(n - l + 1):
-    words.append((i, s[i:i+l]))
+  for i in range(n - word_n + 1):
+    words.append((i, word_n, sorted(s[i:i+word_n])))
   return words
 
 def get_words(s, n):
   words = []
-  l = 1
-  while l < n: 
-    words.extend(get_substrings(s, l, n))
-    l += 1
+  word_n = 1
+  while word_n < n: 
+    words.extend(get_substrings(s, word_n, n))
+    word_n += 1
   return words
 
-def solution(s):
-  result = []
+def sherlockAndAnagrams(s):
+  count = 0
   n = len(s)
   words = get_words(s, n)
-  for i, word in words:
-    word_n = len(word)
+  for i, word_n, word in words:
     for j in range(i + 1, n - word_n + 1):
-      if sorted(word) == sorted(s[j:j+word_n]):
-        result.append((word, s[j:j+word_n]))
-  return len(result)
+      if word == sorted(s[j:j+word_n]):
+        count += 1
+  return count
 
 
 if __name__ == '__main__':
-  assert [(0, 'a'), (1, 'b'), (2, 'b'), (3, 'a')] == get_substrings('abba', 1, len('abba'))
-  assert [(0, 'ab'), (1, 'bb'), (2, 'ba')] == get_substrings('abba', 2, len('abba'))
-  assert [(0, 'abb'), (1, 'bba')] == get_substrings('abba', 3, len('abba'))
-  print(solution('abba'))
-  print(solution('kkkk'))
-  print(solution('ifailuhkqq'))
+  print(sherlockAndAnagrams('abba'))
+  print(sherlockAndAnagrams('kkkk'))
+  print(sherlockAndAnagrams('ifailuhkqq'))
