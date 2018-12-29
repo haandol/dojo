4ae0b47027d2a51e3173f327065cfea456db34d2
vincent <ldg55d@gmail.com>
2018-12-29 12:43:01 +0900

    add knapsack

--- practice/knapsack.py
+++ practice/knapsack.py
@@ -1,16 +1,18 @@
 # https://www.geeksforgeeks.org/0-1-knapsack-problem-dp-10/
 
-def knapsack(W, V, cap, n):
-  if n == 0:
+def knapsack(W, V, cap, i, cache):
+  if i == len(V):
     return 0
 
-  if W[n-1] > cap:
-    return knapsack(W, V, cap, n-1)
-  else:
-    return max(
-      V[n-1] + knapsack(W, V, cap - W[n-1], n-1),
-      knapsack(W, V, cap, n-1)
-    )
+  ret = cache[cap][i]
+  if not ret:
+    ret = knapsack(W, V, cap, i+1, cache)
+  
+  if W[i] <= cap:
+    ret = max(ret, V[i] + knapsack(W, V, cap-W[i], i+1, cache))
+
+  cache[cap][i] = ret
+  return ret
 
 
 if __name__ == '__main__':
@@ -18,4 +20,5 @@ if __name__ == '__main__':
   W = [10, 20, 30]
   cap = 50
   n = len(V)
-  assert 220 == knapsack(W, V, cap, n)
+  cache = [[0] * (n+1) for _ in range(cap+1)]
+  assert 220 == knapsack(W, V, cap, 0, cache)
