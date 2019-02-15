a41364b205130077b8edc4a54c579c529fbdf09d
Lee <dongklee@ea.com>
2019-02-15 18:54:04 +0900

    apply dp to cover distance

--- practice/cover_distance.py
+++ practice/cover_distance.py
@@ -1,14 +1,13 @@
 def solve(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    elif n == 2:
-        return 2
-    elif n == 3:
-        return 4
+    dp = [0] * (n+1)
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
 
-    return solve(n-1) + solve(n-2) + solve(n-3)
+    for i in range(4, n+1):
+        dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+
+    return dp[n]
 
 
-print(solve(4))
+print(solve(7))
