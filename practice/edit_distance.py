630a35ca3f58c2f1d485b3988e0aefca0897d607
Lee <dongklee@ea.com>
2019-02-15 15:35:11 +0900

    apply dp to edit_distance

--- practice/edit_distance.py
+++ practice/edit_distance.py
@@ -1,22 +1,27 @@
-def helper(s1, s2, m, n):
-    if m == 0:
-        return n
-
-    if n == 0:
-        return m
-
-    if s1[m] == s2[n]:
-        return helper(s1, s2, m-1, n-1)
-    else:
-        return 1 + min(
-            helper(s1, s2, m, n-1),
-            helper(s1, s2, m-1, n),
-            helper(s1, s2, m-1, n-1)
-        )
-
-
 def solve(s1, s2):
-    return helper(s1, s2, len(s1)-1, len(s2)-1)
+    m = len(s1)
+    n = len(s2)
+    dp = [
+        [0] * (n+1)
+        for _ in range(m+1)
+    ]
+
+    for i in range(m+1):
+        for j in range(n+1):
+            if i == 0:
+                dp[i][j] = j
+            elif j == 0:
+                dp[i][j] = i
+            elif s1[i-1] == s2[j-1]:
+                dp[i][j] = dp[i-1][j-1]
+            else:
+                dp[i][j] = 1 + min(
+                    dp[i][j-1],
+                    dp[i-1][j],
+                    dp[i-1][j-1]
+                )
+
+    return dp[m][n]
 
 
 assert 1 == solve('geek', 'gesek')
