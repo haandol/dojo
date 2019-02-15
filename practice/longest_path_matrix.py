390754486e4c0f0e45500cf132b651e8231fa539
vincent <ldg55d@gmail.com>
2019-02-15 23:44:08 +0900

    add longest path matrix

--- practice/longest_path_matrix.py
+++ practice/longest_path_matrix.py
@@ -1,3 +1,6 @@
+# https://www.geeksforgeeks.org/find-the-longest-path-in-a-matrix-with-given-constraints/
+
+
 def helper(matrix, row, col):
     directions = [
         [-1, 0],
@@ -27,8 +30,22 @@ def solve(matrix):
 
 
 matrix = [
+    [1, 1],
+    [1, 1]
+]
+assert 1 == solve(matrix)
+
+matrix = [
     [1, 2, 9],
     [5, 3, 8],
     [4, 6, 7]
 ]
-print(solve(matrix))
+assert 4 == solve(matrix)
+
+matrix = [
+    [1, 2, 9, 10],
+    [3, 5, 8, 8],
+    [9, 4, 7, 6],
+    [1, 2, 4, 5]
+]
+assert 7 == solve(matrix)
