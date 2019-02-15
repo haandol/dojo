8e75dd7302fcb5838c65d40f51216df5665a9a25
Lee <dongklee@ea.com>
2019-02-15 13:35:33 +0900

    add eggdrop

--- practice/eggdrop.py
+++ practice/eggdrop.py
@@ -1,15 +1,26 @@
 def eggdrop(e, f):
-  if f == 0 or f == 1:
-    return f
+    dp = [
+        [0] * (f+1)
+        for _ in range(e+1)
+    ]
 
-  if e == 1:
-    return f
+    for i in range(e+1):
+        dp[i][0] = 0
+        dp[i][1] = 0
 
-  trials = float('inf')
-  for i in range(1, f+1):
-    res = max(eggdrop(e-1, i-1), eggdrop(e, f-i))
-    trials = min(trials, res)
+    for i in range(1, f+1):
+        dp[1][i] = i
 
-  return trials + 1
+    for i in range(2, e+1):
+        for j in range(2, f+1):
+            dp[i][j] = float('inf')
+            for k in range(1, j):
+                res = 1 + max(dp[i-1][k-1], dp[i][j-k])
+                dp[i][j] = min(dp[i][j], res)
 
-print(eggdrop(2, 10))
+    return dp[e][f]
+
+
+if __name__ == '__main__':
+    print(eggdrop(2, 10))
+    print(eggdrop(2, 36))
