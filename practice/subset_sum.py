b2c3c36072fc7ceccf44e2e711837e03efbfea44
vincent <ldg55d@gmail.com>
2019-02-16 12:43:31 +0900

    apply dp subset sum

--- practice/subset_sum.py
+++ practice/subset_sum.py
@@ -9,7 +9,26 @@ def helper(arr, n, s):
 
 
 def solve(arr, s):
-    return helper(arr, len(arr), s)
+    n = len(arr)
+    dp = [
+        [False] * (s+1)
+        for _ in range(n+1)
+    ]
+
+    for i in range(n+1):
+        dp[i][0] = True
+
+    for i in range(1, s+1):
+        dp[0][i] = False
+
+    for i in range(1, n+1):
+        for j in range(1, s+1):
+            if j < arr[i-1]:
+                dp[i][j] = dp[i-1][j]
+            else:
+                dp[i][j] = dp[i-1][j] or dp[i-1][j-arr[i-1]]
+
+    return dp[n][s]
 
 
 assert solve([3, 34, 4, 12, 5, 2], 9)
