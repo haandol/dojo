dd846ba8d8ab01a3d81fbb820fa86beb34d08db5
haandol <ldg55d@gmail.com>
2019-02-16 19:39:00 +0900

    apply dp to same subset partition

--- practice/same_subset_partition.py
+++ practice/same_subset_partition.py
@@ -8,13 +8,31 @@ def helper(arr, n, s):
 
 
 def solve(arr):
-    n = len(arr)
     s = sum(arr)
     if s % 2 == 1:
         return False
 
-    return helper(arr, n, sum(arr) // 2)
+    n = len(arr)
+    dp = [
+        [False] * (s//2+1)
+        for _ in range(n+1)
+    ]
+
+    for i in range(n):
+        dp[i][0] = True
+
+    for i in range(1, n+1):
+        for j in range(1, s//2+1):
+            if arr[i-1] == j:
+                dp[i][j] = True
+            elif j < arr[i-1]:
+                dp[i][j] = dp[i-1][j]
+            else:
+                dp[i][j] = dp[i-1][j] or dp[i-1][j - arr[i-1]]
+
+    return dp[n][s//2]
 
 
+assert solve([3, 1, 1, 2, 2, 1])
 assert solve([1, 5, 11, 5])
 assert not solve([1, 5, 3])
