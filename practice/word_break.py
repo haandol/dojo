576dfe8ba5bcddf50c11e31286d9b71f15b25e5e
vincent <ldg55d@gmail.com>
2019-02-17 22:35:50 +0900

    apply dp to word break

--- practice/word_break.py
+++ practice/word_break.py
@@ -11,11 +11,21 @@ def helper(D, s, i):
 
 
 def solve(D, s):
-    return helper(D, s, 0)
+    n = len(s)
+    dp = [False] * (n+1)
+
+    for i in range(n+1):
+        for j in range(1, n+1):
+            if s[i:j] in D:
+                if i == 0 or dp[j-(j-i)]:
+                    dp[j] = True
+
+    return dp[n]
 
 
 D = [
-    'i', 'like', 'sam', 'sung', 'samsung', 'mobile', 'ice', 'cream', 'icecream',
-    'man', 'go', 'mango'
+    'i', 'like', 'sam', 'sung', 'samsung', 'mobile', 'ice', 'cream',
+    'icecream', 'man', 'go', 'mango'
 ]
-print(solve(D, 'ilikesamsung'))
+print(solve(sorted(D, key=lambda x: len(x), reverse=True), 'ilikesamsung'))
+print(solve(sorted(D, key=lambda x: len(x), reverse=True), 'ilikemangoice'))
