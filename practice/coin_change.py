6c95a86fcee7099e8d92281d684c1be2ceefa8e4
vincent <ldg55d@gmail.com>
2019-02-17 20:31:41 +0900

    apply dp to coin change

--- practice/coin_change.py
+++ practice/coin_change.py
@@ -1,18 +1,35 @@
 # https://www.geeksforgeeks.org/coin-change-dp-7/
 
 
-def helper(S, C, n):
-    if C == 0:
+def helper(arr, c, n):
+    if c == 0:
         return 1
-    if C < 0:
+    if c < 0:
         return 0
     if n < 0:
         return 0
-    return helper(S, C, n-1) + helper(S, C-S[n], n)
+    return helper(arr, c, n-1) + helper(arr, c-arr[n], n)
 
 
-def solve(S, C):
-    return helper(S, C, len(S)-1)
+def solve(arr, c):
+    n = len(arr)
+    dp = [
+        [0] * (c+1)
+        for _ in range(n+1)
+    ]
+
+    for i in range(n+1):
+        dp[i][0] = 1
+
+    for i in range(1, n+1):
+        for j in range(1, c+1):
+            dp[i][j] = dp[i-1][j]
+            if 0 <= j-arr[i-1]:
+                dp[i][j] += dp[i][j-arr[i-1]]
+
+    return dp[n][c]
 
 
-print(solve([1, 2, 3], 4))
+assert 4 == solve([1, 2, 3], 4)
+assert 2 == solve([1, 5, 10], 8)
+assert 4 == solve([1, 5, 10], 10)
