36f612a6c89b14e7a4523a209a2122d537603ec3
haandol <ldg55d@gmail.com>
2019-02-20 14:43:00 +0900

    modify reverse

--- practice/reverse_string.py
+++ practice/reverse_string.py
@@ -2,30 +2,12 @@ import re
 
 
 def reverse(s):
-    n = len(s)
-    on_tag = True
-    tag = []
-    res = []
-    for i in range(n-1, -1, -1):
-        if s[i] == '>':
-            tag.append(s[i])
-            on_tag = True
-        elif s[i] == '<':
-            tag.append(s[i])
-            on_tag = False
-            res.extend(tag[::-1])
-            tag = []
-        elif on_tag:
-            tag.append(s[i])
-        else:
-            res.append(s[i])
-    return ''.join(res)
-
+    return s[::-1]
 
 def fix_tag(s):
-    pat = r'(<\w+>)(\w+)(</\w+>)'
-    return re.sub(pat, r'\g<3>\g<2>\g<1>', s)
+    pat = r'>(\w+)\/<(\w+)>(\w+)<'
+    return re.sub(pat, r'<\g<3>>\g<2></\g<1>>', s)
 
 
 s = 'Amazon TV <em>Alexa</em> And <b>Echo</b>'
-print(reverse(fix_tag(s)))
+print(fix_tag(reverse(s)))
