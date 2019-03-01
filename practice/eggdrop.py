41d0f8d0237dfd1224bd275c77635fa1e6920f39
haandol <ldg55d@gmail.com>
2019-03-01 17:12:36 +0900

    add eggdrop recursive

--- practice/eggdrop.py
+++ practice/eggdrop.py
@@ -1,26 +1,24 @@
-def eggdrop(e, f):
-    dp = [
-        [0] * (f+1)
-        for _ in range(e+1)
-    ]
+def helper(e, f):
+    if e == 1:
+        return f
+    if f < 2:
+        return f
 
-    for i in range(e+1):
-        dp[i][0] = 0
-        dp[i][1] = 0
-
+    min_so_far = float('inf')
     for i in range(1, f+1):
-        dp[1][i] = i
+        res = max(helper(e-1, i-1), helper(e, f-i))
+        min_so_far = min(min_so_far, res)
+    
+    return min_so_far + 1
 
-    for i in range(2, e+1):
-        for j in range(2, f+1):
-            dp[i][j] = float('inf')
-            for k in range(1, j):
-                res = 1 + max(dp[i-1][k-1], dp[i][j-k])
-                dp[i][j] = min(dp[i][j], res)
 
-    return dp[e][f]
+def eggdrop(e, f):
+    return helper(e, f)
 
 
 if __name__ == '__main__':
-    print(eggdrop(2, 10))
-    print(eggdrop(2, 36))
+    assert 2 == eggdrop(1, 2)
+    assert 2 == eggdrop(2, 2)
+    assert 3 == eggdrop(2, 6)
+    assert 4 == eggdrop(2, 7)
+    assert 4 == eggdrop(3, 14)
