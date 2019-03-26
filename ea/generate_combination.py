81872e6fcf7c4aa86d6bebf65090c9671abb09e4
Lee <dongklee@ea.com>
2019-03-26 18:56:51 +0900

    make same number of letters place first

--- ea/generate_combination.py
+++ ea/generate_combination.py
@@ -1,20 +1,26 @@
 from pprint import pprint
 
 
-def helper(s, path, res):
+def helper(s, path, k, res):
   if not s:
+    for item in path:
+      if k == len(item):
+        res.insert(0, path[:])
+        return
+
     res.append(path[:])
     return
 
   n = len(s)
+
   for length in range(1, n+1):
-    helper(s[length:], path+[s[:length]], res)
+    helper(s[length:], path+[s[:length]], k, res)
 
 
-def solve(s):
+def solve(s, k):
   res = []
-  helper(s, [], res)
+  helper(s, [], k, res)
   return res
 
 
-pprint(solve('DDMF'))
+pprint(solve('TJMAS', 2))
